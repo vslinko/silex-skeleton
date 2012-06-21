39527053a71887a34b6f95db6e26acd40c5376b4
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-06-21 18:58:19 +0400

    Fixed deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,6 @@ set :deploy_to, "/var/www/#{application}"
 
 namespace :deploy do
     task :finalize_update do
-        run "cd #{deploy_to} && wget -q http://getcomposer.org/composer.phar && php ./composer.phar install"
+        run "cd #{release_path} && wget -q http://getcomposer.org/composer.phar && php ./composer.phar install"
     end
 end
